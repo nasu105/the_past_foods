cb2268337ecbee232e01a400d6bbe6a655fd9cf6
nasu105 <fuyuyasai105@gmail.com>
2022-05-19 02:12:54 +0900

    update

--- memo_text_read.php
+++ memo_text_read.php
@@ -17,6 +17,7 @@ if ($file) {
 		for($i = 0; $i < count($line); $i++){
 			// echo"<td>".$line[$i]." </td>";
 			$str .= "<td>".$line[$i]." </td>";
+			array_push($array, $str);
 			// $str .= "<tr><td>{$line[$i]}</td></tr>";
 		}
 		$str .= "</tr>";
@@ -31,12 +32,15 @@ fclose($file);
 
 ?>
 
+<!-- htmlを書いていくだお -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 	<title>過去飯一覧</title>
 </head>
 <body>
@@ -49,6 +53,12 @@ fclose($file);
 				</tbody>
 			</table>
 	</fieldset>
-    
+	<div>
+		<!-- 円グラフ作成のためのcanvasを用意 -->
+		<canvas id="mycanvas" height="450" width="800"></canvas>
+	</div>
+
+	<!-- javascriptを読み込むだお -->
+	<script src="main.js"></script>
 </body>
 </html>
